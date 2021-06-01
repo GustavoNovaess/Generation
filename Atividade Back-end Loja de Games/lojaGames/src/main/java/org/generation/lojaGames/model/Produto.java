270dd093880e98ab8784dcde7f6648ca4497c888
Gustavo <gustavo.novaes@aluno.ufabc.edu.br>
2021-06-01 11:32:13 -0300

    CRUD de uma API de Loja de Games - Atividade GeneratiON

---
+++ Atividade Back-end Loja de Games/lojaGames/src/main/java/org/generation/lojaGames/model/Produto.java
@@ -0,0 +1,88 @@
+package org.generation.lojaGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 1, max = 100)
+	private String nome;
+	
+	@NotNull
+	private double preco;
+	
+	@NotNull
+	@Size(min = 1, max = 155)
+	private String plataforma;
+	
+	@NotNull
+	private int anoLancamento;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public int getAnoLancamento() {
+		return anoLancamento;
+	}
+
+	public void setAnoLancamento(int anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
---
+++ Atividade Back-end Loja de Games/lojaGames/src/main/java/org/generation/lojaGames/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package org.generation.lojaGames.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import org.generation.lojaGames.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+}
